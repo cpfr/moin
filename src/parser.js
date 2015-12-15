f7eccb944e6a48ea02a59d72720f7d847cdd6e35
Carsten Pfeffer <carsten.pfeffer@uni-bremen.de>
2015-12-15 09:23:19 +0100

    allowing comments and empty lines from the first line of a program

--- src/parser.js
+++ src/parser.js
@@ -623,26 +623,40 @@ var mtyParser = (function() {
     }
 
     function peg$parsemoduleDeclaration() {
-      var s0, s1, s2;
+      var s0, s1, s2, s3;
 
       s0 = peg$currPos;
       s1 = [];
-      s2 = peg$parseclassDeclaration();
-      if (s2 === peg$FAILED) {
-        s2 = peg$parsestatement();
-      }
+      s2 = peg$parseEmptyLineOrComment();
       while (s2 !== peg$FAILED) {
         s1.push(s2);
-        s2 = peg$parseclassDeclaration();
-        if (s2 === peg$FAILED) {
-          s2 = peg$parsestatement();
-        }
+        s2 = peg$parseEmptyLineOrComment();
       }
       if (s1 !== peg$FAILED) {
-        peg$savedPos = s0;
-        s1 = peg$c0(s1);
+        s2 = [];
+        s3 = peg$parseclassDeclaration();
+        if (s3 === peg$FAILED) {
+          s3 = peg$parsestatement();
+        }
+        while (s3 !== peg$FAILED) {
+          s2.push(s3);
+          s3 = peg$parseclassDeclaration();
+          if (s3 === peg$FAILED) {
+            s3 = peg$parsestatement();
+          }
+        }
+        if (s2 !== peg$FAILED) {
+          peg$savedPos = s0;
+          s1 = peg$c0(s2);
+          s0 = s1;
+        } else {
+          peg$currPos = s0;
+          s0 = peg$FAILED;
+        }
+      } else {
+        peg$currPos = s0;
+        s0 = peg$FAILED;
       }
-      s0 = s1;
 
       return s0;
     }
